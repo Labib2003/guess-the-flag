eae532c2c313718f77e488c9d9f0d97991635882
Labib2003 <labib.salimi30@gmail.com>
2022-02-24 21:47:58 +0600

    name reaviling added

--- js/app.js
+++ js/app.js
@@ -7,13 +7,26 @@ const loadData = () => {
 loadData();
 
 const displayData = (data) => {
-    const countryGrid = document.getElementById('country-grid');
+    const flagGrid = document.getElementById('flag-grid');
     data.forEach(country => {
-        console.log(country);
+        // console.log(country);
         const flag = document.createElement('div');
+        flag.classList.add('country-container');
         flag.innerHTML = `
-            <img src = ${country.flags.svg}>
-        `
-        countryGrid.appendChild(flag);
+            <img class="flag-img" src = ${country.flags.svg}>
+        `;
+        const revealBtn = document.createElement('button');
+        revealBtn.innerText = 'Reveal name';
+        flag.appendChild(revealBtn);
+        revealBtn.addEventListener('click', () => {
+            const h3 = document.createElement('h3');
+            h3.innerText = country.name;
+            flag.removeChild(revealBtn);
+            flag.appendChild(h3);
+        });
+        flagGrid.appendChild(flag);
     });
+};
+const validateName = () => {
+    console.log(document.getElementById('input').value);
 }
